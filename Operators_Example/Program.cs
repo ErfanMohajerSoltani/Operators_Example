07d3cd480b9ccd925c65dba456dad1e239d9f5bf
Erfan Mohajer Soltani <erfanm512@gmail.com>
2022-02-28 01:00:21 +0330

    using operators and print the result of them

--- Operators_Example/Program.cs
+++ Operators_Example/Program.cs
@@ -6,7 +6,21 @@ namespace Operators_Example
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int firstNumber = 6;
+            int secondNumber = 14;
+            int plusPlus = firstNumber++;
+            int minusMinus = secondNumber--;
+            int plus = firstNumber + secondNumber;
+            int minus = firstNumber - secondNumber;
+            int multiplication = plusPlus * minus;
+            int division = secondNumber / plusPlus;
+
+            Console.WriteLine("plusPlus equals to: " + plusPlus);
+            Console.WriteLine("minusMinus equals to: " + minusMinus);
+            Console.WriteLine("plus equals to: " + plus);
+            Console.WriteLine("minus equals to: " + minus);
+            Console.WriteLine("multiplication equals to: " + multiplication);
+            Console.WriteLine("division equals to: " + division);
         }
     }
 }
