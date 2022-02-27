d7884e66828ab9966344395883e8a7193d3616ce
Erfan Mohajer Soltani <erfanm512@gmail.com>
2022-02-28 00:49:14 +0330

    Add project files.

---
+++ Operators_Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operators_Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
